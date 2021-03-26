2928ad4595fb33d3c0fd01654a5853b291ec237e
danielvazquez-utp <daniel.vazquez@utpuebla.edu.mx>
2021-03-25 21:16:04 -0600

    Plantillas genericas agregadas

---
+++ application/views/Plantillas/head_view.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>FoodMathLab | Blank Page</title>
+  <!-- Icono -->
+  <link rel="icon" type="img/icon" href="<?= base_url('vendor') ?>/dist/img/logos/nutrimotor-logo-bn.png">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= base_url('vendor') ?>/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?= base_url('vendor') ?>/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?= base_url('vendor') ?>/dist/css/adminlte.min.css">
+</head>
---
+++ application/views/Plantillas/navbar_view.php
@@ -0,0 +1,30 @@
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Left navbar links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="#" class="nav-link text-red"><i class="fas fa-house-user"></i> Inicio</a>
+    </li>
+  </ul>
+
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
+    
+    <li class="nav-item">
+      <a class="nav-link" data-widget="fullscreen" href="#" role="button" title="Pantalla completa">
+        <i class="fas fa-expand-arrows-alt"></i>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="#" role="button" title="Salir">
+        <i class="fas fa-sign-out-alt"></i>
+      </a>
+    </li>
+
+  </ul>
+</nav>
+<!-- /.navbar -->
